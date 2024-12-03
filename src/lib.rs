d4128ea65d5660f3843bbf9d0a2fac26482f3372
Wolf McNally <wolf@wolfmcnally.com>
2024-12-02 18:43:32 -0800

    Added docs version checking.

--- src/lib.rs
+++ src/lib.rs
@@ -44,3 +44,16 @@ pub use key::*;
 
 mod delegate;
 pub use delegate::*;
+
+#[cfg(test)]
+    mod tests {
+    #[test]
+    fn test_readme_deps() {
+        version_sync::assert_markdown_deps_updated!("README.md");
+    }
+
+    #[test]
+    fn test_html_root_url() {
+        version_sync::assert_html_root_url_updated!("src/lib.rs");
+    }
+}

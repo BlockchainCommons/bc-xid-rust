f2468a77abadecd5a9b8fa0d702143ed49ad437d
Wolf McNally <wolf@wolfmcnally.com>
2024-12-02 18:20:06 -0800

    Added docs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,32 @@
+#![doc(html_root_url = "https://docs.rs/bc-xid/0.1.0")]
+#![warn(rust_2018_idioms)]
+
+//! # Introduction
+//!
+//! XIDs (eXtensible IDentity, */zid/*) are unique 32-byte identifier that
+//! represent any entities—real or abstract—such as a person, organization, or
+//! device. Generated from the SHA-256 hash of a specific public signing key
+//! known as the inception key, a XID provides a stable identity throughout its
+//! lifecycle, even as associated keys and permissions evolve. Leveraging
+//! Gordian Envelope for XID Documents, XIDs are recursively resolvable and
+//! extensible, allowing for detailed assertions about the entity, including key
+//! declarations, permissions, controllers, and endpoints. The integration of
+//! [provenance marks](https://provemark.com) ensures a verifiable chain of
+//! document revisions, enhancing security and authenticity in decentralized
+//! identity management.
+//!
+//! # Getting Started
+//!
+//! ```toml
+//! [dependencies]
+//! bc-xid = "0.1.0"
+//! ```
+//!
+//! # Examples
+//!
+//! See the unit tests in the source code for examples of how to use this
+//! library.
+
 mod privilege;
 pub use privilege::*;
 

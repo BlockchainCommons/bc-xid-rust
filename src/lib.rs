40a1e73629505b2927d71c511d3a1391ddd1cfe4
Wolf McNally <wolf@wolfmcnally.com>
2024-12-03 10:16:28 -0800

    Bumped version number.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/bc-xid/0.1.1")]
+#![doc(html_root_url = "https://docs.rs/bc-xid/0.1.2")]
 #![warn(rust_2018_idioms)]
 
 //! # Introduction
@@ -19,7 +19,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! bc-xid = "0.1.1"
+//! bc-xid = "0.1.2"
 //! ```
 //!
 //! # Examples

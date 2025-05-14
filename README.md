b32c2df720ce40b5895ffffec3c1f4870b80a3b1
Wolf McNally <wolf@wolfmcnally.com>
2025-05-14 11:19:56 -0700

    Prep for release

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ XIDs (eXtensible IDentity, _/zid/_) are unique 32-byte identifier that represent
 
 ```toml
 [dependencies]
-bc-xid = "0.7.1"
+bc-xid = "0.8.0"
 ```
 
 ## Specification
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/bc-xid/0.7.1")]
+#![doc(html_root_url = "https://docs.rs/bc-xid/0.8.0")]
 #![warn(rust_2018_idioms)]
 
 //! # Introduction
@@ -19,7 +19,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! bc-xid = "0.7.1"
+//! bc-xid = "0.8.0"
 //! ```
 //!
 //! # Examples

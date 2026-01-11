6ad2f743845a11664ee2f4f96b0fb59a909c63b6
Wolf McNally <wolf@wolfmcnally.com>
2026-01-11 15:47:01 -0700

    Prep for release

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/bc-xid/0.19.0")]
+#![doc(html_root_url = "https://docs.rs/bc-xid/0.20.0")]
 #![warn(rust_2018_idioms)]
 
 //! # Introduction
@@ -19,7 +19,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! bc-xid = "0.19.0"
+//! bc-xid = "0.20.0"
 //! ```
 //!
 //! # Examples
